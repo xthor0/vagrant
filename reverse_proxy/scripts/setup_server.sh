0ce92abee31c25d0d598944724c80e2a33616bee
Ben Brown <xthor@xthorsworld.com>
2018-10-24 08:00:54 -0600

    a few minor updates

--- reverse_proxy/scripts/setup_server.sh
+++ reverse_proxy/scripts/setup_server.sh
@@ -30,18 +30,15 @@ esac
 case "$1" in
     proxy)
         yum -y install nginx && systemctl enable nginx
-        cat /srv/config/nginx.conf | tee /etc/nginx/nginx.conf >& /dev/null
-        mkdir -p /srv/www && chcon -t httpd_sys_content_t /srv/www
-        cat /srv/config/index.html | tee /srv/www/index.html >& /dev/null
+        rm -f /etc/nginx/nginx.conf 
+        ln -s /srv/config/nginx.conf /etc/nginx/nginx.conf
         systemctl start nginx
         ;;
     webapp*)
         yum -y install nodejs
-        mkdir -p /srv/www 
-        cp /srv/scripts/simpleweb.js /srv/www/
         cat /srv/config/simpleweb.service | tee /etc/systemd/system/simpleweb.service >& /dev/null
         sed -i "s/%%port%%/${NODEPORT}/g" /etc/systemd/system/simpleweb.service
-        sed -i "s/%%hostname%%/${HOSTNAME}/g" /etc/systemd/system/simpleweb.service
+        sed -i "s/%%hostname%%/${1}/g" /etc/systemd/system/simpleweb.service
         systemctl daemon-reload
         systemctl enable simpleweb && systemctl start simpleweb
         ;;
